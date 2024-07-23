f85767e33ddfce11d68d148dcace2ed687496d2b
Vincekool2 <glassb450m@gmail.com>
2024-07-23 11:27:40 +0200

    paths

--- src/Controller/categoriesController.php
+++ src/Controller/categoriesController.php
@@ -1,31 +1,29 @@
 <?php
 
+
 //Indique le chemin d'accès du fichier
 namespace App\Controller;
+
 //Fait le lien avec les classes / equivalent du require_once mais avec les Namespaces
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 //Ceci est une classe. AbstractController est en heritage pour utiliser ses fonctionnalités
-class ArticleController extends AbstractController
+class categoriesController extends AbstractController
 {
-
     //Permet de créer une nouvelle page avec l'adresse indiquée
-    #[Route('/categories', name: 'list_category')]
-    public function listCategory(){
+    #[Route('/cate', name: 'cate')]
+    public function index(){
         $categories = [
             'Red', 'Green', 'Blue', 'Yellow', 'Gold', 'Silver', 'Crystal'
         ];
 
-        $html=$this->renderview('page/Category.html.twig', [
+        $html=$this->renderview('categories.html.twig', [
             'categories' => $categories
         ]);
         //Retourne un status 200
         return new Response($html, 200);
     }
 }
-
-
-
